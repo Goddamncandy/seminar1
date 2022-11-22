6154a2ac40d362dc2bc3c9054b11c7e265c93dc1
Vera <akane@inbox.ru>
2022-11-22 11:56:16 +0400

    Решение второй домашней задачи

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+{
+    Console.WriteLine(A);
+}
+else 
+{
+    Console.WriteLine(B);
+}
