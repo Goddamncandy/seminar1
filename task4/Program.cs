f761601f342370ee545220ec81e25a0949b01036
Vera <akane@inbox.ru>
2022-11-22 12:06:41 +0400

    Решение домашней задачи 4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int C = Convert.ToInt32(Console.ReadLine());
+if (A > B && A > C)
+{
+    Console.WriteLine(A);
+}
+else if (B > C)
+{
+    Console.WriteLine(B);
+}
+else
+{
+    Console.WriteLine(C);
+}
